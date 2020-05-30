073780d8f654eefcba9beb07f783862e6aa96025
Ahmet AYDIN <ahmetaydi@migros.com.tr>
2020-05-30 16:20:50 +0300

    ngx-toast package added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'udemy-angular-course';
+export class AppComponent implements OnInit {
+  title = "udemy-angular-course";
+  constructor(private toastr: ToastrService) {}
+
+  ngOnInit() {
+    this.showSuccess();
+  }
+
+  showSuccess() {
+    this.toastr.success(
+      "Udemy Angular 9 projemize hoşgeldiniz...!",
+      "Giriş Başarılı!"
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { ToastrModule } from "ngx-toastr";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      progressBar: true,
+      easing: "ease-in",
+      closeButton: false,
+      progressAnimation: "decreasing",
+      preventDuplicates: true,
+      positionClass: "toast-bottom-left"
+    }) // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
