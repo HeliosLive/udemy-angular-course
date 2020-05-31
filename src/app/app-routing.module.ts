350192bd3600a931e72d82a784d455a71f970f5c
Ahmet AYDIN <ahmetaydi@migros.com.tr>
2020-05-31 21:57:57 +0300

    local storage and cookie added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,11 @@ const routes: Routes = [
       )
   },
   {
+    path: "storage",
+    loadChildren: () =>
+      import("./storage/storage.module").then(m => m.StorageModule)
+  },
+  {
     path: "**",
     component: HomeComponent
   }
