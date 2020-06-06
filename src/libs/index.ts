76f3e3462eecbade43b7845eeb8813735a1cc59e
Ahmet AYDIN <ahmetaydi@migros.com.tr>
2020-06-06 18:22:24 +0300

    angular ngForm &  reactive form added

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -5,3 +5,9 @@ export * from "./custom-pipes/my-filtering.pipe";
 
 export * from "./child-components/child.module";
 export * from "./child-components/child1/child1.component";
+
+export * from "./custom-validators/blacklist.validator";
+export * from "./custom-validators/no-white-space.validator";
+export * from "./custom-validators/user-name-exist.validator";
+
+export * from "./services/user.service";
