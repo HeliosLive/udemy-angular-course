7cba4e2db0316f6aec2d17e7d3c4d8046952ac2d
Ahmet AYDIN <ahmetaydi@migros.com.tr>
2020-07-05 01:09:47 +0300

    ngx translate added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
 import { NgxSpinnerService } from "ngx-spinner";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-root",
@@ -11,8 +12,12 @@ export class AppComponent implements OnInit {
   title = "udemy-angular-course";
   constructor(
     private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-  ) {}
+    private spinner: NgxSpinnerService,
+    public translateService: TranslateService
+  ) {
+    translateService.setDefaultLang("en");
+    translateService.use("en");
+  }
 
   ngOnInit() {
     this.showSuccess();
@@ -20,10 +25,11 @@ export class AppComponent implements OnInit {
   }
 
   showSuccess() {
-    this.toastr.success(
-      "Udemy Angular 9 projemize hoşgeldiniz...!",
-      "Giriş Başarılı!"
-    );
+    this.translateService.onLangChange.subscribe(resp => {
+      const title = this.translateService.instant("toastr.title");
+      const message = this.translateService.instant("toastr.message");
+      this.toastr.success(title, message);
+    });
   }
 
   showSpinner() {
@@ -33,5 +39,9 @@ export class AppComponent implements OnInit {
       /** spinner ends after 5 seconds */
       this.spinner.hide();
     }, 1000);
+  }
+
+  setLanguage(lang: string) {
+    this.translateService.use(lang);
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from "./home.component";
 import { Routes, RouterModule } from "@angular/router";
 import { IgxButtonModule } from "igniteui-angular";
 
+import { HttpClient } from "@angular/common/http";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
 const routes: Routes = [
   {
     path: "",
@@ -13,6 +17,25 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, IgxButtonModule, RouterModule.forChild(routes)]
+  imports: [
+    CommonModule,
+    IgxButtonModule,
+    RouterModule.forChild(routes),
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ]
 })
 export class HomeModule {}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(
+    http,
+    "./../../assets/i18n/",
+    ".json?cb=" + new Date().getTime()
+  );
+}
